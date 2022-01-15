d4c1300628b0eb92752af23b9cf9c50a21a273dc
eepj <62833867+eepj@users.noreply.github.com>
2022-01-15 17:53:54 +0800

    Port functions from MAX30100 lib

--- max30102_for_stm32_hal.c
+++ max30102_for_stm32_hal.c
@@ -15,7 +15,110 @@ void max30102_write(max30102_t *obj, uint8_t reg, uint8_t *buf, size_t buflen)
     free(payload);
 }
 
-void max30102_read(max30102_t *obj, uint8_t *buf, size_t buflen)
+void max30102_read(max30102_t *obj, uint8_t reg, uint8_t *buf, size_t buflen)
 {
+    uint8_t reg_addr = reg;
+    HAL_I2C_Master_Transmit(obj->ui2c, MAX30102_I2C_ADDR << 1, &reg_addr, buflen + 1, 1000);
     HAL_I2C_Master_Receive(obj->ui2c, MAX30102_I2C_ADDR << 1, buf, buflen, 1000);
+}
+
+void max30102_enable_interrupt(max30102_t *obj, uint8_t a_full, uint8_t ppg_rdy, uint8_t alc_ovf, uint8_t die_temp_rdy)
+{
+    uint8_t interrupt_enabled[2] = {0x00};
+    interrupt_enabled[0] |= 1 << MAX30102_INTERRUPT_A_FULL;
+    interrupt_enabled[0] |= 1 << MAX30102_INTERRUPT_PPG_RDY;
+    interrupt_enabled[0] |= 1 << MAX30102_INTERRUPT_ALC_OVF;
+    interrupt_enabled[1] |= 1 << MAX30102_INTERRUPT_DIE_TEMP_RDY;
+    max30102_write(obj, MAX30102_INTERRUPT_ENABLE_1, interrupt_enabled, 2);
+}
+
+void max30102_interrupt_handler(max30102_t *obj)
+{
+    uint8_t interrupt[2] = {0x00};
+    max30102_read(obj, MAX30102_INTERRUPT_STATUS_1, interrupt, 2);
+    // To be implemented
+}
+
+void max30102_shutdown(max30102_t *obj, uint8_t shdn)
+{
+    uint8_t config;
+    max30102_read(obj, MAX30102_MODE_CONFIG, &config, 1);
+    config = (config & ~(1 << MAX30102_MODE_SHDN)) | (shdn << MAX30102_MODE_SHDN);
+    max30102_write(obj, MAX30102_MODE_CONFIG, &config, 1);
+}
+
+void max30102_set_mode(max30102_t *obj, max30102_mode_t mode)
+{
+    uint8_t config;
+    obj->_mode = mode;
+    max30102_read(obj, MAX30102_MODE_CONFIG, &config, 1);
+    config = (config & 0xf8) | mode;
+    max30102_write(obj, MAX30102_MODE_CONFIG, &config, 1);
+}
+
+void max30102_set_spo2_sampling_rate(max30102_t *obj, max30102_spo2_sr_t sr)
+{
+    uint8_t config;
+    max30102_read(obj, MAX30102_SPO2_CONFIG, &config, 1);
+    config = (config & 0x63) << MAX30102_SPO2_SR;
+    max30102_write(obj, MAX30102_SPO2_CONFIG, &config, 1);
+}
+
+void max30102_set_spo2_led_pulse_width(max30102_t *obj, max30102_spo2_led_pw_t pw)
+{
+    uint8_t config;
+    max30102_read(obj, MAX30102_SPO2_CONFIG, &config, 1);
+    config = (config & 0x7c) | (pw << MAX30102_SPO2_LEW_PW);
+    max30102_write(obj, MAX30102_SPO2_CONFIG, &config, 1);
+}
+void max30102_set_spo2_adc_resolution(max30102_t *obj, max30102_spo2_adc_t adc)
+{
+    uint8_t config;
+    max30102_read(obj, MAX30102_SPO2_CONFIG, &config, 1);
+    config = (config & 0x1f) | (adc << MAX30102_SPO2_ADC_RGE);
+    max30102_write(obj, MAX30102_SPO2_CONFIG, &config, 1);
+}
+
+void max30102_set_led_current_1(max30102_t *obj, float ma)
+{
+    if (ma < 0)
+        ma = 0;
+    else if (ma > 51.0)
+        ma = 51.0;
+    uint8_t pa = ma / 0.2;
+    max30102_write(obj, MAX30102_LED_PA1, &pa, 1);
+}
+
+void max30102_set_led_current_2(max30102_t *obj, float ma)
+{
+    if (ma < 0)
+        ma = 0;
+    else if (ma > 51.0)
+        ma = 51.0;
+    uint8_t pa = ma / 0.2;
+    max30102_write(obj, MAX30102_LED_PA2, &pa, 1);
+}
+
+void max30102_clear_fifo(max30102_t *obj)
+{
+    uint8_t val = 0x00;
+    max30102_write(obj, MAX30102_FIFO_WR_PTR, &val, 1);
+    max30102_write(obj, MAX30102_FIFO_RD_PTR, &val, 1);
+    max30102_write(obj, MAX30102_OVF_COUNTER, &val, 1);
+}
+
+void max30102_read_fifo(max30102_t *obj)
+{
+    uint8_t ptr[3] = {0x00};
+    max30102_read(obj, MAX30102_FIFO_WR_PTR, ptr, 1);
+
+    uint8_t number_of_samples = ptr[2] - ptr[0];
+    uint8_t fifo_data[64] = {0};
+    max30102_read(obj, MAX30102_FIFO_DATA, fifo_data, number_of_samples);
+
+    for (uint8_t i = 0; i < number_of_samples; i += 4)
+    {
+        obj->_ir_sample[i / 4] = (fifo_data[i] << 8) | fifo_data[i + 1];
+        obj->_red_sample[i / 4] = (fifo_data[i + 2] << 8) | fifo_data[i + 3];
+    }
 }
--- max30102_for_stm32_hal.h
+++ max30102_for_stm32_hal.h
@@ -1,20 +1,84 @@
 #ifndef MAX30102_FOR_STM32_HAL_H
 #define MAX30102_FOR_STM32_HAL_H
 
-#define MAX30102_I2C_ADDR 0x57
-#define MAX30102_I2C_TIMEOUT 1000
-
 #include "../Inc/main.h"
 #include <stdint.h>
 #include <string.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define MAX30102_I2C_ADDR 0x57
+#define MAX30102_I2C_TIMEOUT 1000
+
+#define MAX30102_INTERRUPT_STATUS_1 0x00
+#define MAX30102_INTERRUPT_STATUS_2 0x01
+#define MAX30102_INTERRUPT_ENABLE_1 0x02
+#define MAX30102_INTERRUPT_ENABLE_2 0x03
+#define MAX30102_INTERRUPT_A_FULL           7
+#define MAX30102_INTERRUPT_PPG_RDY          6
+#define MAX30102_INTERRUPT_ALC_OVF          5
+#define MAX30102_INTERRUPT_DIE_TEMP_RDY     1
+
+#define MAX30102_FIFO_WR_PTR        0x04
+#define MAX30102_OVF_COUNTER        0x05
+#define MAX30102_FIFO_RD_PTR        0x06
+
+#define MAX30102_FIFO_DATA          0x07
+
+#define MAX30102_MODE_CONFIG        0x09
+#define MAX30102_MODE_SHDN                  7
+#define MAX30102_MODE_RESET                 6
+#define MAX30102_MODE_MODE                  0
+
+#define MAX30102_SPO2_CONFIG        0x0a
+#define MAX30102_SPO2_ADC_RGE               5
+#define MAX30102_SPO2_SR                    2
+#define MAX30102_SPO2_LEW_PW                0
+
+#define MAX30102_LED_PA1            0x0c
+#define MAX30102_LED_PA2            0x0d
+
+typedef enum max30102_mode_t {
+    max30102_heart_rate = 0x02,
+    max30102_SpO2 = 0x03,
+    max30102_multi_led = 0x07
+} max30102_mode_t;
+
+typedef enum max30102_spo2_sr_t {
+    max30102_spo2_50,
+    max30102_spo2_100,
+    max30102_spo2_200,
+    max30102_spo2_400,
+    max30102_spo2_800,
+    max30102_spo2_1000,
+    max30102_spo2_1600,
+    max30102_spo2_3200
+} max30102_spo2_sr_t;
+
+typedef enum max30102_spo2_led_pw_t {
+    max30102_spo2_15_bit,
+    max30102_spo2_16_bit,
+    max30102_spo2_17_bit,
+    max30102_spo2_18_bit
+} max30102_spo2_led_pw_t;
+
+typedef enum max30102_spo2_adc_t {
+    max30102_spo2_adc_00,
+    max30102_spo2_adc_01,
+    max30102_spo2_adc_10,
+    max30102_spo2_adc_11
+} max30102_spo2_adc_t;
 
 typedef struct max30102_t
 {
     I2C_HandleTypeDef *ui2c;
+    max30102_mode_t _mode;
+    uint16_t _ir_sample[16];
+    uint16_t _red_sample[16];
 } max30102_t;
 
-void max30102_init(max30102_t *obj, I2C_HandleTypeDef *hi2c); 
+void max30102_init(max30102_t *obj, I2C_HandleTypeDef *hi2c);
 void max30102_write(max30102_t *obj, uint8_t reg, uint8_t *buf, size_t buflen);
-void max30102_read(max30102_t *obj, uint8_t *buf, size_t buflen);
+void max30102_read(max30102_t *obj, uint8_t reg, uint8_t *buf, size_t buflen);
 
 #endif

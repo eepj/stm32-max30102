c47bc084909ff97c0c9fd176a89267516dd0b8ee
eepj <62833867+eepj@users.noreply.github.com>
2022-01-15 15:39:46 +0800

    Add implementation of I2C read/write func

--- max30102_for_stm32_hal.c
+++ max30102_for_stm32_hal.c
@@ -1,1 +1,21 @@
 #include "max30102_for_stm32_hal.h"
+
+void max30102_init(max30102_t *obj, I2C_HandleTypeDef *hi2c)
+{
+    obj->ui2c = hi2c;
+}
+
+void max30102_write(max30102_t *obj, uint8_t reg, uint8_t *buf, size_t buflen)
+{
+    uint8_t *payload = (uint8_t *)malloc((buflen + 1) * sizeof(uint8_t));
+    *payload = reg;
+    if (buf != NULL && buflen != 0)
+        memcpy(payload + 1, buf, buflen);
+    HAL_I2C_Master_Transmit(obj->ui2c, MAX30102_I2C_ADDR << 1, payload, buflen + 1, 1000);
+    free(payload);
+}
+
+void max30102_read(max30102_t *obj, uint8_t *buf, size_t buflen)
+{
+    HAL_I2C_Master_Receive(obj->ui2c, MAX30102_I2C_ADDR << 1, buf, buflen, 1000);
+}
--- max30102_for_stm32_hal.h
+++ max30102_for_stm32_hal.h
@@ -2,13 +2,19 @@
 #define MAX30102_FOR_STM32_HAL_H
 
 #define MAX30102_I2C_ADDR 0x57
+#define MAX30102_I2C_TIMEOUT 1000
 
-typedef struct max30102
+#include "../Inc/main.h"
+#include <stdint.h>
+#include <string.h>
+
+typedef struct max30102_t
 {
-    
-} max30102;
+    I2C_HandleTypeDef *ui2c;
+} max30102_t;
 
-void max30102_write(uint8_t reg, uint8_t *payload);
-void max30102_read(uint8_t reg, uint8_t *payload);
+void max30102_init(max30102_t *obj, I2C_HandleTypeDef *hi2c); 
+void max30102_write(max30102_t *obj, uint8_t reg, uint8_t *buf, size_t buflen);
+void max30102_read(max30102_t *obj, uint8_t *buf, size_t buflen);
 
 #endif

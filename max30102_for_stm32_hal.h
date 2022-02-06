1e018bb5abb502f02118448833cd933f2284fef2
eepj <62833867+eepj@users.noreply.github.com>
2022-02-06 13:21:45 +0800

    Add comments

--- max30102_for_stm32_hal.h
+++ max30102_for_stm32_hal.h
@@ -76,7 +76,7 @@ typedef enum max30102_smp_ave_t
     max30102_smp_ave_32,
 } max30102_smp_ave_t;
 
-typedef enum max30102_spo2_sr_t
+typedef enum max30102_sr_t
 {
     max30102_spo2_50,
     max30102_spo2_100,
@@ -86,23 +86,23 @@ typedef enum max30102_spo2_sr_t
     max30102_spo2_1000,
     max30102_spo2_1600,
     max30102_spo2_3200
-} max30102_spo2_sr_t;
+} max30102_sr_t;
 
-typedef enum max30102_spo2_led_pw_t
+typedef enum max30102_led_pw_t
 {
     max30102_spo2_15_bit,
     max30102_spo2_16_bit,
     max30102_spo2_17_bit,
     max30102_spo2_18_bit
-} max30102_spo2_led_pw_t;
+} max30102_led_pw_t;
 
-typedef enum max30102_spo2_adc_t
+typedef enum max30102_adc_t
 {
-    max30102_spo2_adc_2048,
-    max30102_spo2_adc_4096,
-    max30102_spo2_adc_8192,
-    max30102_spo2_adc_16384
-} max30102_spo2_adc_t;
+    max30102_adc_2048,
+    max30102_adc_4096,
+    max30102_adc_8192,
+    max30102_adc_16384
+} max30102_adc_t;
 
 typedef enum max30102_multi_led_ctrl_t
 {
@@ -140,10 +140,10 @@ void max30102_interrupt_handler(max30102_t *obj);
 void max30102_shutdown(max30102_t *obj, uint8_t shdn);
 
 void max30102_set_mode(max30102_t *obj, max30102_mode_t mode);
-void max30102_set_sampling_rate(max30102_t *obj, max30102_spo2_sr_t sr);
+void max30102_set_sampling_rate(max30102_t *obj, max30102_sr_t sr);
 
-void max30102_set_led_pulse_width(max30102_t *obj, max30102_spo2_led_pw_t pw);
-void max30102_set_adc_resolution(max30102_t *obj, max30102_spo2_adc_t adc);
+void max30102_set_led_pulse_width(max30102_t *obj, max30102_led_pw_t pw);
+void max30102_set_adc_resolution(max30102_t *obj, max30102_adc_t adc);
 
 void max30102_set_led_current_1(max30102_t *obj, float ma);
 void max30102_set_led_current_2(max30102_t *obj, float ma);

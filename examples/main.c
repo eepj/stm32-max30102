8f44792f27af0e97907f262b454ef489d9a68ecd
Pan <62833867+eepj@users.noreply.github.com>
2022-02-11 08:48:09 +0800

    Override plot function in example

--- examples/main.c
+++ examples/main.c
@@ -47,13 +47,22 @@ I2C_HandleTypeDef hi2c1;
 UART_HandleTypeDef huart1;
 
 /* USER CODE BEGIN PV */
-// printf function
+// printf() function
 int __io_putchar(int ch)
 {
   uint8_t temp = ch;
   HAL_UART_Transmit(&huart1, &temp, 1, HAL_MAX_DELAY);
   return ch;
 }
+
+// Override plot function
+void max30102_plot(uint32_t ir_sample, uint32_t red_sample)
+{
+    // printf("ir:%u\n", ir_sample);                  // Print IR only
+    // printf("r:%u\n", red_sample);                  // Print Red only
+    printf("ir:%u,r:%u\n", ir_sample, red_sample);    // Print IR and Red
+}
+
 // MAX30102 object
 max30102_t max30102;
 /* USER CODE END PV */
@@ -103,24 +112,27 @@ int main(void)
   MX_I2C1_Init();
   MX_USART1_UART_Init();
   /* USER CODE BEGIN 2 */
+  // Initiation
   max30102_init(&max30102, &hi2c1);
   max30102_reset(&max30102);
   max30102_clear_fifo(&max30102);
   max30102_set_fifo_config(&max30102, max30102_smp_ave_8, 1, 7);
-
+  
+  // Sensor settings
   max30102_set_led_pulse_width(&max30102, max30102_pw_16_bit);
   max30102_set_adc_resolution(&max30102, max30102_adc_2048);
   max30102_set_sampling_rate(&max30102, max30102_sr_800);
-
   max30102_set_led_current_1(&max30102, 6.2);
   max30102_set_led_current_2(&max30102, 6.2);
 
   // Enter SpO2 mode
   max30102_set_mode(&max30102, max30102_spo2);
   max30102_set_a_full(&max30102, 1);
+  
   // Initiate 1 temperature measurement
   max30102_set_die_temp_en(&max30102, 1);
   max30102_set_die_temp_rdy(&max30102, 1);
+  
   uint8_t en_reg[2] = {0};
   max30102_read(&max30102, 0x00, en_reg, 1);
   /* USER CODE END 2 */
